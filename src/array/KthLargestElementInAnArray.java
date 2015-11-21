7b6c98571b0ccfa340cb7e008def349f9cd71d94
Shuai Wang <shuaiwhu@gmail.com>
2015-11-21 20:13:51 +0800

    First commit

---
+++ src/array/KthLargestElementInAnArray.java
@@ -0,0 +1,19 @@
+package array;
+
+public class KthLargestElementInAnArray {
+	
+	public int findKthLargest(int[] nums, int k) {
+        int len = nums.length;
+        
+    }
+	
+	private void partition(int[] nums, int k){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/list/DeleteNodeInALinkedList.java
@@ -0,0 +1,21 @@
+package list;
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) { val = x; }
+}
+
+public class DeleteNodeInALinkedList {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
