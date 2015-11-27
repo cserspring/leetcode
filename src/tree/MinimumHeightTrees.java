cd6a09acf06711373def585da8d3f2923c28519b
Shuai Wang <shuaiwhu@gmail.com>
2015-11-27 23:17:00 +0800

    O(N) time to get MHTs

--- src/tree/MinimumHeightTrees.java
+++ src/tree/MinimumHeightTrees.java
@@ -1,55 +1,45 @@
 package tree;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 public class MinimumHeightTrees {
-
-    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-    	int x;
-        if (edges == null || (x = edges.length) == 0) return null;
-        if (edges[0] == null || edges[0].length == 0) return null;
-                
-    	ArrayList<ArrayList<Integer>> adjacencyLists = new ArrayList<ArrayList<Integer>>(n);
+	// We remove the leaves one step by one step, so there will be at most 2 vertices at last.
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {         
+    	if (n == 1) return Collections.singletonList(0);
+    	
+    	ArrayList<HashSet<Integer>> adjacencyLists = new ArrayList<HashSet<Integer>>(n);
         for (int i = 0; i < n; ++i) {
-        	adjacencyLists.add(new ArrayList<Integer>());
+        	adjacencyLists.add(new HashSet<Integer>());
         }
         
-        for (int i = 0; i < x; ++i) {
-        	adjacencyLists.get(edges[i][0]).add(edges[i][1]);
-        	adjacencyLists.get(edges[i][1]).add(edges[i][0]);
+        for (int[] edge : edges) {
+        	adjacencyLists.get(edge[0]).add(edge[1]);
+        	adjacencyLists.get(edge[1]).add(edge[0]);
         }
         
         List<Integer> result = new ArrayList<Integer>();
-        int minHeight = Integer.MAX_VALUE;
         for (int i = 0; i < n; ++i) {
-        	HashSet<Integer> visited = new HashSet<Integer>();
-        	int value = heightBasedOnVertex(i, adjacencyLists, visited);
-        	if (value == minHeight) {
+        	if (adjacencyLists.get(i).size() == 1)
         		result.add(i);
-        	} else if (value < minHeight) {
-        		result.clear();
-        		result.add(i);
-        		minHeight = value;
-        	}
+        }
+
+        while (n > 2) {
+			n = n - result.size();
+			List<Integer> newResult = new ArrayList<Integer>();
+			for (int i : result) {
+				int label = adjacencyLists.get(i).iterator().next();
+				adjacencyLists.get(label).remove(i);
+				if (adjacencyLists.get(label).size() == 1)
+					newResult.add(label);
+			}
+
+        	result = newResult;
         }
         
         return result;
-    }
-    
-    private int heightBasedOnVertex(int label, ArrayList<ArrayList<Integer>> adjacencyLists, HashSet<Integer> visited) {
-    	if (visited.contains(label))
-    		return 0;
-    	int max = 0;
-   		visited.add(label);
-   		for (int i : adjacencyLists.get(label)) {
-   			int value = heightBasedOnVertex(i, adjacencyLists, visited);
-   			if (value > max) {
-   				max = value;
-   			}
-   		}
-   		return max + 1;
     }
     
 	public static void main(String[] args) {
