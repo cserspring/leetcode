59240593c16f21574ca54aed653cbaa5fe245f1a
Shuai Wang <shuaiwhu@gmail.com>
2015-12-14 20:35:35 +0800

    O(N) solution of Container With Most Water

--- src/area/ContainerWithMostWater.java
+++ src/area/ContainerWithMostWater.java
@@ -2,6 +2,25 @@ package area;
 
 public class ContainerWithMostWater {
 
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int maxArea = 0;
+        int i = 0;
+        int j = n - 1;
+        while (i < j) {
+        	int area = Math.min(height[i], height[j]) * (j - i);
+        	if (height[i] <= height[j]) 
+        		++i;
+        	else 
+        		--j;        	
+        	
+        	if (area > maxArea)
+        		maxArea = area;
+        }
+        
+        return maxArea;
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
