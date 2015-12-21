4775d15ae067f16f62a904ad90a60401d9785154
Shuai Wang <shuaiwhu@gmail.com>
2015-12-21 10:21:26 +0800

    Divide two integers

--- src/manipulation/DivideTwoIntegers.java
+++ src/manipulation/DivideTwoIntegers.java
@@ -7,21 +7,36 @@ public class DivideTwoIntegers {
         if (divisor == 1) return dividend;
         if (divisor == -1) return -dividend;
         
-        int a = Math.abs(dividend);
-        int b = Math.abs(divisor);
-        int delta = a - b;
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        
+        long base = b;
+        int i = 0;
+        long[] nums = new long[32];
+        nums[0] = b;
+        while (nums[i] <= a && nums[i] > 0) {
+        	nums[i+1] = base << (i+1);
+        	++i;
+        }
+        --i;
+        
         int cnt = 0;
-        while (delta > 0) {
-        	++cnt;
-        	delta = delta - b;
+        while (a > 0 && i >= 0) {
+        	while (a - nums[i] >= 0) {
+        		a -= nums[i];
+        		cnt += (1 << i);
+        	}
+        	--i;
         }
         
-        return cnt;
+        return ((dividend ^ divisor) >> 31) == 0 ? cnt : -cnt;
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DivideTwoIntegers d = new DivideTwoIntegers();
+		System.out.println(d.divide(-2147483648,
+				2));
 	}
 
 }
