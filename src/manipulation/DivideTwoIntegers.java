a1b55ffce752efeb2076f4dbee97ca7a5a1d990b
Shuai Wang <shuaiwhu@gmail.com>
2015-12-21 21:33:22 +0800

    Divide Two Integers

--- src/manipulation/DivideTwoIntegers.java
+++ src/manipulation/DivideTwoIntegers.java
@@ -35,8 +35,7 @@ public class DivideTwoIntegers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DivideTwoIntegers d = new DivideTwoIntegers();
-		System.out.println(d.divide(-2147483648,
-				2));
+		System.out.println(d.divide(-2147483648, 2));
 	}
 
 }
