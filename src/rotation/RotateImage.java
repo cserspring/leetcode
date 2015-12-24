e89e5721c74a2adfc12bbf4b84cc77bf67ff0dc4
Shuai Wang <shuaiwhu@gmail.com>
2015-12-24 23:09:18 +0800

    Code is more concise for Rotate Image

--- src/rotation/RotateImage.java
+++ src/rotation/RotateImage.java
@@ -5,35 +5,14 @@ public class RotateImage {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         int rows = n / 2;
-        int cols = n % 2 == 0 ? rows : (rows + 1);
-        int[][] tmp = new int[rows][cols];
+        int cols = n - rows;
         for (int i = 0; i < rows; ++i) {
         	for (int j = 0; j < cols; ++j) {
-        		tmp[i][j] = matrix[i][j];
-        	}
-        }
-        
-        for (int i = rows; i < n; ++i) {
-        	for (int j = 0; j < cols; ++j) {
-        		matrix[j][n - 1 - i] = matrix[i][j];
-        	}
-        }
-        
-        for (int i = cols; i < n; ++i) {
-        	for (int j = rows; j < n; ++j) {
-        		matrix[j][n - 1 - i] = matrix[i][j];
-        	}
-        }
-        
-        for (int i = 0; i < cols; ++i) {
-        	for (int j = cols; j < n; ++j) {
-        		matrix[j][n - 1 - i] = matrix[i][j];
-        	}
-        }
-        
-        for (int i = 0; i < rows; ++i) {
-        	for (int j = 0; j < cols; ++j) {
-        		matrix[j][n - 1 - i] = tmp[i][j];
+        		int tmp = matrix[i][j];
+        		matrix[i][j] = matrix[n - 1 - j][i];
+        		matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+        		matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+        		matrix[j][n - 1 - i] = tmp;
         	}
         }
     }
