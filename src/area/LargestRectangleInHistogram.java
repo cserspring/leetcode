8230f8aa7a4e24281edc9fef861b304e27af8e88
Shuai Wang <shuaiwhu@gmail.com>
2016-01-02 22:39:12 +0800

    Fix bug

--- src/area/LargestRectangleInHistogram.java
+++ src/area/LargestRectangleInHistogram.java
@@ -9,18 +9,22 @@ public class LargestRectangleInHistogram {
         int[] right = new int[n];
         for (int pos = 0; pos < n; ++pos) {
         	left[pos] = right[pos] = pos;
-        	for (int i = pos - 1; i >= 0; --i) {
+        	int i = pos - 1;
+        	for (; i >= 0; --i) {
         		if (height[i] < height[pos]) {
         			left[pos] = i + 1;
         			break;
         		}
         	}
-        	for (int i = pos + 1; i < n; ++i) {
+        	if (i == -1) left[pos] = 0;
+        	
+        	for (i = pos + 1; i < n; ++i) {
         		if (height[i] < height[pos]) {
         			right[pos] = i - 1;
         			break;
         		}
         	}
+        	if (i == n) right[pos] = n - 1;
         	int area = height[pos] * (right[pos] - left[pos] + 1);
         	if (area > max)
         		max = area;
@@ -31,7 +35,7 @@ public class LargestRectangleInHistogram {
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] height = { 2,1,5,6,2,3 };
+		int[] height = { 1,1,1,1 };
 		LargestRectangleInHistogram l = new LargestRectangleInHistogram();
 		System.out.println(l.largestRectangleArea(height));
 	}
