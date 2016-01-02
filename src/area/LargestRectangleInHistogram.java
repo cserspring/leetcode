1a176616616193610bbc04b0ffee6d13dc612c8c
Shuai Wang <shuaiwhu@gmail.com>
2016-01-03 01:23:12 +0800

    Largest Rectangle In Histogram O(N)

--- src/area/LargestRectangleInHistogram.java
+++ src/area/LargestRectangleInHistogram.java
@@ -1,41 +1,33 @@
 package area;
 
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
 
     public int largestRectangleArea(int[] height) {
-    	int n = height.length;
+    	Stack<Integer> stack = new Stack<Integer>();
     	int max = 0;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        for (int pos = 0; pos < n; ++pos) {
-        	left[pos] = right[pos] = pos;
-        	int i = pos - 1;
-        	for (; i >= 0; --i) {
-        		if (height[i] < height[pos]) {
-        			left[pos] = i + 1;
-        			break;
-        		}
-        	}
-        	if (i == -1) left[pos] = 0;
-        	
-        	for (i = pos + 1; i < n; ++i) {
-        		if (height[i] < height[pos]) {
-        			right[pos] = i - 1;
-        			break;
-        		}
-        	}
-        	if (i == n) right[pos] = n - 1;
-        	int area = height[pos] * (right[pos] - left[pos] + 1);
-        	if (area > max)
-        		max = area;
-        }
-
-        return max;
+    	int n = height.length;
+    	int[] newHeight = new int[n + 1];
+    	for (int i = 0; i < n; ++i) newHeight[i] = height[i];
+    	newHeight[n] = 0;
+    	int i = 0;
+    	while (i < n + 1) {
+    		if (stack.isEmpty() || newHeight[stack.peek()] <= newHeight[i]) {
+    			stack.push(i++);
+    		} else {
+    			int index = stack.pop();
+    			int area = (stack.isEmpty() ? i : (i - stack.peek() - 1)) * newHeight[index];
+    			max = (max < area ? area : max);
+    		}
+    	}
+    	
+    	return max;
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] height = { 1,1,1,1 };
+		int[] height = { 1,1 };
 		LargestRectangleInHistogram l = new LargestRectangleInHistogram();
 		System.out.println(l.largestRectangleArea(height));
 	}
