fea055886df06c6fc3639ad02e16d48f9f5413f9
Shuai Wang <shuaiwhu@gmail.com>
2016-01-04 23:11:57 +0800

    Unique Binary Search Trees

---
+++ src/bst/UniqueBinarySearchTrees.java
@@ -0,0 +1,23 @@
+package bst;
+
+public class UniqueBinarySearchTrees {
+
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+        	for (int j = 0; j < i; ++j) {
+        		dp[i] += dp[j] * dp[i - 1 - j];
+        	}
+        }
+        return dp[n];
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UniqueBinarySearchTrees u = new UniqueBinarySearchTrees();
+		System.out.println(u.numTrees(3));
+	}
+
+}
