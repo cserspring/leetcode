e7ea36582885f6e62a19d4f6ed55dffa55984f8d
Shuai Wang <shuaiwhu@gmail.com>
2016-01-09 18:22:35 +0800

    Iterative method to solve symmetric tree

--- src/tree/SymmetricTree.java
+++ src/tree/SymmetricTree.java
@@ -1,16 +1,36 @@
 package tree;
 
+import java.util.Stack;
+
 public class SymmetricTree {
 
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
-        return isSymmetric(root.left, root.right);
+        Stack<TreeNode> stack1 = new Stack<TreeNode>();
+        Stack<TreeNode> stack2 = new Stack<TreeNode>();
+        TreeNode p = root.left;
+        TreeNode q = root.right;
+        while ((p != null && q != null) || (!stack1.isEmpty() && !stack2.isEmpty())) {
+        	while (p != null && q != null) {
+        		stack1.push(p);
+        		p = p.left;
+        		stack2.push(q);
+        		q = q.right;
+        	} 
+        	
+        	if (p != null || q != null) return false;
+        	
+        	if (stack1.isEmpty() && stack1.isEmpty()) return true;
+        	if (stack1.isEmpty() || stack1.isEmpty()) return false;
+        	
+        	TreeNode node1 = stack1.pop();
+        	TreeNode node2 = stack2.pop();
+        	if (node1.val != node2.val) return false;
+        	p = node1.right;
+        	q = node2.left;        	
+        }
+        
+        return p == null && q == null && stack1.isEmpty() && stack2.isEmpty();
     }
     
-    private boolean isSymmetric(TreeNode left, TreeNode right) {
-    	if (left == null && right == null) return true;
-    	if (left == null || right == null) return false;
-    	return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
-    }
-
 }
