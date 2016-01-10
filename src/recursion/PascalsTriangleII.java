696edd0b2ff96f726b959f4a6445526c15cacd7f
Shuai Wang <shuaiwhu@gmail.com>
2016-01-11 00:06:39 +0800

    Pascal's Triangle II more concise

--- src/recursion/PascalsTriangleII.java
+++ src/recursion/PascalsTriangleII.java
@@ -6,26 +6,23 @@ import java.util.ArrayList;
 public class PascalsTriangleII {
 
     public List<Integer> getRow(int rowIndex) {
-        
+    	List<Integer> res;
         if (rowIndex == 0) {
-        	List<Integer> res = new ArrayList<Integer>();
+        	res = new ArrayList<Integer>();
         	res.add(1);
-        	return res;
-        } else if (rowIndex > 0) {
-        	List<Integer> res = getRow(rowIndex - 1);
-        	int size = res.size();
+        } else {
+        	res = getRow(rowIndex - 1);
         	int tmp = 0;
-        	int last = 0;
-        	for (int i = 1; i < size; ++i) {
+        	int last = 1;
+        	for (int i = 1; i < res.size(); ++i) {
         		tmp = res.get(i);
         		res.set(i, res.get(i) + last);
         		last = tmp;
         	}
         	res.add(1);
-        	return res;
-        } else {
-        	return new ArrayList<Integer>();
-        }
+        } 
+        
+        return res;
     }
 
 }
