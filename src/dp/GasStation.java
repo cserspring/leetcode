c8298320ce424c9dc6f81dd93ef340b371d50f37
Shuai Wang <shuaiwhu@gmail.com>
2016-07-24 19:17:30 -0700

    Gas Station Simple Code"

--- src/dp/GasStation.java
+++ src/dp/GasStation.java
@@ -5,24 +5,22 @@ public class GasStation {
     	int n;
         if (gas == null || (n = gas.length) == 0) return 0;
         int index = -1;
+        int[] dp = new int[n];
+        for (int i = 0; i < n; ++i)
+        	dp[i] = gas[i] - cost[i];
+        int oil = 0;
+        int total = 0;
         for (int i = 0; i < n; ++i) {
-            int oil = 0;
-            int j;
-        	for (j = i; j < n + i; ++j) {
-        		if (oil + gas[j%n] < cost[j%n]) {
-        			break;
-        		} else {
-        			oil = oil + gas[j%n] - cost[j%n];
-        		}
-        	}
-        	
-        	if ( j == n + i) {
+        	oil += dp[i];
+        	total += dp[i];
+        	if (oil < 0) {
         		index = i;
-        		break;
+        		oil = 0;
         	}
         }
         
-        return index;
+        if (total < 0) return -1;
+        return index + 1;
     }
     
 	public static void main(String[] args) {
