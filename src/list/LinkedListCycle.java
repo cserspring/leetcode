af2f6acb0be80485e7fc30eaf61c98144c3091fd
Shuai Wang <shuaiwhu@gmail.com>
2016-07-28 23:51:22 -0700

    More concise

--- src/list/LinkedListCycle.java
+++ src/list/LinkedListCycle.java
@@ -2,14 +2,13 @@ package list;
 
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
+    	if (head == null || head.next == null) return false;
+    	
         ListNode p1 = head;
         ListNode p2 = head;
-        while (p1 != null && p2 != null) {
+        while (p2.next != null && p2.next.next != null) {
         	p1 = p1.next;
-        	if (p2.next != null) 
-        		p2 = p2.next.next;
-        	else
-        		return false;
+        	p2 = p2.next.next;
         	if (p1 == p2)
         		return true;
         }
