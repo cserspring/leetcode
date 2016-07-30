d7b7c86dd56975eab93cd8ddc4414a388578f275
Shuai Wang <shuaiwhu@gmail.com>
2016-07-30 14:27:09 -0700

    Use stack to do preorder

--- src/treetraversal/BinaryTreePreorderTraversal.java
+++ src/treetraversal/BinaryTreePreorderTraversal.java
@@ -2,20 +2,23 @@ package treetraversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePreorderTraversal {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<Integer>();
-        preorder(res, root);
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+        	TreeNode node = stack.pop();
+        	res.add(node.val);
+        	if (node.right != null)
+        		stack.push(node.right);
+        	if (node.left != null) 
+        		stack.push(node.left);
+        }
         return res;
-    }
-    
-    private void preorder(List<Integer> res, TreeNode root) {
-    	if (root != null) {
-    		res.add(root.val);
-    		preorder(res, root.left);
-    		preorder(res, root.right);
-    	}
     }
     
 	public static void main(String[] args) {
