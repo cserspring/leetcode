0b463c9fa19ea836e3202dc5aef65d724d8b95ac
Shuai Wang <shuaiwhu@gmail.com>
2016-07-31 13:50:23 -0700

    Use Two Stacks to Solve PostOrder

--- src/treetraversal/BinaryTreePostorderTraversal.java
+++ src/treetraversal/BinaryTreePostorderTraversal.java
@@ -2,20 +2,27 @@ package treetraversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePostorderTraversal {
     public List<Integer> postorderTraversal(TreeNode root) {
     	List<Integer> res = new ArrayList<Integer>();
-    	postOrder(res, root);
-    	return res;
-    }
-    
-    private void postOrder(List<Integer> res, TreeNode root) {
-    	if (root != null) {
-    		postOrder(res, root.left);
-    		postOrder(res, root.right);
-    		res.add(root.val);
+    	Stack<TreeNode> stack1 = new Stack<TreeNode>();
+    	Stack<TreeNode> stack2 = new Stack<TreeNode>();
+    	if (root != null) stack1.push(root);
+    	while (!stack1.isEmpty()) {
+    		TreeNode node = stack1.pop();
+    		stack2.push(node);
+    		if (node.left != null) 
+    			stack1.push(node.left);
+    		if (node.right != null)
+    			stack1.push(node.right);
     	}
+    	
+    	while (!stack2.isEmpty()) 
+    		res.add(stack2.pop().val);
+    	
+    	return res;
     }
     
 	public static void main(String[] args) {
