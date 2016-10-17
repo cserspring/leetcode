df1dc1461694892e374077fed219fea1cab7629c
Shuai Wang <shuaiwhu@gmail.com>
2016-10-16 17:08:57 -0700

    Unique Binary Search Trees II

--- src/bst/UniqueBinarySearchTreesII.java
+++ src/bst/UniqueBinarySearchTreesII.java
@@ -13,40 +13,37 @@ class TreeNode {
 public class UniqueBinarySearchTreesII {
 
     public List<TreeNode> generateTrees(int n) {
-        List<TreeNode> res = new ArrayList<TreeNode>();
-        if (n <= 0) return res;
+    	if (n <= 0) return new ArrayList<TreeNode>();
         return generateTrees(1, n);
     }
     
     private List<TreeNode> generateTrees(int left, int right) {
     	List<TreeNode> roots = new ArrayList<TreeNode>();
-    	if (left > right || left <= 0 || right <= 0) {
+    	if (left > right) {
     		roots.add(null);
-    	} else if (left == right) {
-    		TreeNode root = new TreeNode(left);
-    		roots.add(root);
-    	} else {
-    		for (int i = left; i <= right; ++i) {
-    			List<TreeNode> leftTree = generateTrees(left, i - 1);
-    			List<TreeNode> rightTree = generateTrees(i + 1, right);
-    			for (int j = 0; j < leftTree.size(); ++j) {
-    				for (int k = 0; k < rightTree.size(); ++k) {
-    					TreeNode root = new TreeNode(i);
-    					root.left = leftTree.get(j);
-    					root.right = rightTree.get(k);
-    					roots.add(root);
-    				}
+    		return roots;
+    	}
+    	
+    	for (int i = left; i <= right; ++i) {
+    		List<TreeNode> leftTree = generateTrees(left, i - 1);
+    		List<TreeNode> rightTree = generateTrees(i + 1, right);
+    		for (int m = 0; m < leftTree.size(); ++m) {
+    			for (int n = 0; n < rightTree.size(); ++n) {
+    				TreeNode root = new TreeNode(i);
+    				root.left = leftTree.get(m);
+    				root.right = rightTree.get(n);
+    				roots.add(root);
     			}
     		}
     	}
     	
-    	return roots;    	
+    	return roots;
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UniqueBinarySearchTreesII u = new UniqueBinarySearchTreesII();
-		u.generateTrees(3);
+		/*List<TreeNode> nodes = */u.generateTrees(3);
 	}
 
 }
