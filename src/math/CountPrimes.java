25d2d1df86ba5b5b72734c2168991d2e3fe878b9
Shuai Wang <shuaiwhu@gmail.com>
2016-10-20 23:08:08 -0700

    Count Primes

--- src/math/CountPrimes.java
+++ src/math/CountPrimes.java
@@ -3,27 +3,25 @@ package math;
 public class CountPrimes {
     public int countPrimes(int n) {
     	int count = 0;
-        for (int i = 2; i < n; ++i) {
-        	if (isPrime(i)) ++count;
+        boolean[] flag = new boolean[n];
+        for (int i = 2; i < n; ++i) flag[i] = true;
+        for (int i = 2; i <= n/i; ++i) {
+        	if (!flag[i]) continue;
+        	for (int j = i * i; j < n; j = j + i) {
+        		flag[j] = false;
+        	}
         }
         
+        for (int i = 2; i < n; ++i) 
+        	if (flag[i])
+        		++count;
         return count;
-    }
-    
-    private boolean isPrime(int n) {
-    	if (n <= 1) return false;
-    	if (n == 2 || n == 3) return true;
-    	if (n % 2 == 0 || n % 3 == 0) return false;
-    	for (int i = 3; i * i <= n; i = i + 2) {
-    		if (n % i == 0) return false;
-    	}
-    	
-    	return true;
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		CountPrimes c = new CountPrimes();
+		System.out.println(c.countPrimes(5));
 	}
 
 }
