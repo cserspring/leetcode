3bdec4cdb4e857d52f1428b4cf64e48501f6d323
Shuai Wang <shuaiwhu@gmail.com>
2016-10-24 23:18:23 -0700

    Rectangle Area

--- src/math/RectangleArea.java
+++ src/math/RectangleArea.java
@@ -4,11 +4,12 @@ public class RectangleArea {
 
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
     	int area = (C - A) * (D - B) + (G - E) * (H - F);
+    	if (C <= E || G <= A || F >= D || B >= H) return area;
     	int top = Math.min(D, H);
     	int bottom = Math.max(B, F);
     	int left = Math.max(A, E);
     	int right = Math.min(C, G);
-    	return area - Math.max(0, right - left) * Math.max(top - bottom, 0);
+    	return area - (right - left) * (top - bottom);
     }
     
 	public static void main(String[] args) {
