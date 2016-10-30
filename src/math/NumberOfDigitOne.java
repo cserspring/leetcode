a05d99b6ec6b6ab724741296bb122f66f26c8b92
Shuai Wang <shuaiwhu@gmail.com>
2016-10-30 16:19:12 -0700

    Convert to String Still TimeOut

--- src/math/NumberOfDigitOne.java
+++ src/math/NumberOfDigitOne.java
@@ -4,26 +4,19 @@ public class NumberOfDigitOne {
     public int countDigitOne(int n) {
     	int count = 0;
         for (int i = 1; i <= n; ++i) {
-        	count += numOfDigitOne(i);
+        	String s = String.valueOf(i);
+        	for (int j = 0; j < s.length(); ++j) 
+        		if (s.charAt(j) == '1')
+        			++count;
         }
         
         return count;
     }
     
-    private int numOfDigitOne(int n) {
-    	int count = 0;
-    	while (n > 0) {
-    		if (n % 10 == 1) ++count;
-    		n = n / 10;
-    	}
-    	
-    	return count;
-    }
-    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		NumberOfDigitOne n = new NumberOfDigitOne();
-		System.out.println(n.countDigitOne(824883294));
+		System.out.println(n.countDigitOne(13));
 	}
 
 }
