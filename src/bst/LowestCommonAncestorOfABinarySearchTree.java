eb3ffceb16bd83d101c379aed9651311b8f0383b
Shuai Wang <shuaiwhu@gmail.com>
2016-10-30 18:00:15 -0700

    Lowest Common Ancestor of a Binary Search Tree

---
+++ src/bst/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,18 @@
+package bst;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    	if (root == null) return null;
+    	if (root.val > p.val && root.val > q.val)
+    		return lowestCommonAncestor(root.left, p, q);
+    	if (root.val < p.val && root.val < q.val)
+    		return lowestCommonAncestor(root.right, p, q);
+    	return root;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
