f88d8b2faef9cfa6977f503f4690932ebd4ffb0b
Shuai Wang <shuaiwhu@gmail.com>
2016-10-30 17:33:34 -0700

    Second Solution to Solve Palindrome Linked List

--- src/list/PalindromeLinkedList.java
+++ src/list/PalindromeLinkedList.java
@@ -4,37 +4,19 @@ public class PalindromeLinkedList {
 	// http://www.programcreek.com/2014/07/leetcode-palindrome-linked-list-java/
 	// Solution 1: Reverse the second half
 	// Solution 2: Recursive
+	private ListNode left;
+	
     public boolean isPalindrome(ListNode head) {
-    	if (head == null) return true;
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast.next != null && fast.next.next != null) {
-        	slow = slow.next;
-        	fast = fast.next.next;
-        }
-        
-        ListNode secondHead = reverse(slow.next);
-        slow.next = null;
-        while (head != null && secondHead != null) {
-        	if (head.val != secondHead.val)
-        		return false;
-        	head = head.next;
-        	secondHead = secondHead.next;
-        }
-        
-        return true;
+    	left = head;
+    	return helper(head);
     }
     
-    private ListNode reverse(ListNode head) {
-    	ListNode prev = null;
-    	while (head != null) {
-    		ListNode next = head.next;
-    		head.next = prev;
-    		prev = head;
-    		head = next;
-    	}
-    	
-    	return prev;
+    private boolean helper(ListNode right) {
+    	if (right == null) return true;
+    	if (!helper(right.next)) return false;
+    	boolean res = (left.val == right.val);
+    	left = left.next;
+    	return res;
     }
     
 	public static void main(String[] args) {
